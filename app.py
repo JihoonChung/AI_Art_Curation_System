1a89e898a53018130652256a50f4b35c854f6985
Anson Lau <anson.lau@live.com>
2024-03-27 22:59:29 -0400

    Update app to include autoencoder, convert autoencoder/rec to modules

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
 from transformers import BlipProcessor, BlipForConditionalGeneration
+import joblib
+import rec as rc
+
+# Autoencoder for similar images
+
+
 
 # Define a random model for testing purposes
 class RandomModel(nn.Module):
@@ -54,6 +60,10 @@ def preprocess_image(image):
 
     return input_tensor
 
+def get_model(path):
+    best_model = joblib.load('model\\resnet50_wo_l2_epoch3.joblib')
+    return best_model
+
 def captioner(image, style):
     # Load BLIP
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
@@ -85,11 +95,15 @@ def predict(image):
          # Create a dictionary to store class probabilities
         style_probabilities = {styles[i]: probabilities_style[0][i].item() for i in range(len(styles))}
         genre_probabilities = {genres[i]: probabilities_genre[0][i].item() for i in range(len(genres))}
-        most_likely_style = max(style_probabilities, key=style_probabilities.get)
-        most_likely_genre = max(genre_probabilities, key=genre_probabilities.get)
-        caption = captioner(image['composite'], most_likely_style)
-
-    return style_probabilities, genre_probabilities, caption
+        #most_likely_style = max(style_probabilities, key=style_probabilities.get)
+        #most_likely_genre = max(genre_probabilities, key=genre_probabilities.get)
+    sim_img, title = rc.get_similar_image(input_image = input_tensor, 
+                           pickle_file_path='./outputs/encoded_features_desc_w_Cluster_unique.pkl', 
+                           kmeans_model_path='./outputs/kmeans_model.joblib', 
+                           unet_model_path='./outputs/UNet_SEMART/model_epoch_2.pth',
+                           search_directory="E:\\SemArt\\Images")
+    
+    return style_probabilities, genre_probabilities, sim_img, title
 
 # Flagging (Feedback)
 callback = gr.CSVLogger()
@@ -110,24 +124,30 @@ with gr.Blocks(theme=gr.themes.Soft(), title="AI Art Curation System") as demo:
             with gr.Column():
                 input_image = gr.ImageEditor(type='pil', image_mode='RGB', transforms='crop', eraser=False, brush=False,scale=2, label="Input")
                 btn = gr.Button("Generate")
+                reset_btn = gr.Button("Reset")
             with gr.Column():
                 output_style = gr.Label(label="Predicted Style",num_top_classes=3,scale=0)
                 output_genre = gr.Label(label="Predicted Genre",num_top_classes=3,scale=0)
+                output_text = gr.Textbox(label="Suggested Artwork Title",scale=0,show_copy_button=True,max_lines=2)
             with gr.Column():
-                output_caption = gr.Text(label="Generated Caption",scale=0,show_copy_button=True)
-        btn.click(predict, inputs=input_image, outputs=[output_style, output_genre, output_caption])
+                output_image = gr.Image(label="Recommended Artwork",scale=0, type="pil", interactive=False,height=512, width=512)
+                #output_caption = gr.Text(label="Generated Caption",scale=0,show_copy_button=True)
+        btn.click(predict, inputs=input_image, outputs=[output_style, output_genre, output_image, output_text])
+        reset_btn.click(inputs=[])
    
     with gr.Tab("Feedback"): # Feedback tab
         with gr.Column():
             with gr.Row():
                 style_rating = gr.Dropdown(styles+['None'], label="Corrected Style")
                 genre_rating = gr.Dropdown(genres+['None'], label="Corrected Genre")
-            caption_rating = gr.Slider(1, 10, step = 1, value=5, label="Caption Rating", info="Rate the Generated Caption Between 1 to 10 (Best)")
+            image_rating = gr.Slider(1, 10, step = 1, value=5, label="Recommendation Rating", info="Rate the Recommended Artwork Between 1 to 10 (Best)")
+            #caption_rating = gr.Slider(1, 10, step = 1, value=5, label="Caption Rating", info="Rate the Generated Caption Between 1 to 10 (Best)")
             btn_feedback = gr.Button("Flag Feedback")
-    flagged_content = [input_image, output_style, output_genre, output_caption, style_rating, genre_rating, caption_rating]
+    flagged_content = [input_image, output_style, output_genre, output_image, style_rating, genre_rating, image_rating]
     callback.setup(flagged_content, flagging_dir="flagged_data") # This needs to be called at some point prior to the first call to callback.flag()
     btn_feedback.click(lambda *args: callback.flag(args), inputs=flagged_content, outputs=None, preprocess=False)
 
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(share=False)
+    #demo.launch(auth=('admin','MIE1517'),share=True)
